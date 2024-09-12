5dc2b84eb0d5392cb36799b1479eaa9263e62d6b
KairuOwO <canlasedsonkyle@gmail.com>
2024-09-12 14:42:50 +0800

    Add subtraction method

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int quo = a / b;
+        int mod = a % b;
+
+        System.out.println("Addition: " + sum );
+        System.out.println("Subtraction: " + sub );
+        System.out.println("Multiplication: " + mul );
+        System.out.println("Division: " + quo );
+        System.out.println("Modulus: " + mod );
+
+
+
+
+    }
+}
